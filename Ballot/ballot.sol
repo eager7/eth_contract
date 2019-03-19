a22f6269d55f0b7c8622fc5ec29af07e655626d2
eager7 <panchangtao@gmail.com>
2019-03-19 16:54:16 +0800

    contract

---
+++ Ballot/ballot.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.22;
+
+///@title vote
+contract ballot {
+    struct Voter {
+        uint    weight;
+        bool    voted;
+        address delegate;
+        uint    vote;
+    }
+
+    struct Proposal {
+        bytes32 name;
+        uint    voteCount;
+    }
+
+    address public chairPerson;
+
+    mapping(address => Voter) public voters;
+
+    Proposal[] public proposals;
+
+    function ballot(){
+
+    }
+}
--- helloworld/hello.sol
+++ helloworld/hello.sol
@@ -1,7 +1,30 @@
 pragma solidity ^0.5.0;
 
 contract HelloWorld {
+    function HelloWorld() public {
+    }
+
+    function() public {
+    }
+
+    // External functions
+    // ...
+
+    // External functions that are view
+    // ...
+
+    // External functions that are pure
+    // ...
     function helloWorld() external pure returns (string memory) {
         return "Hello, World!";
     }
+    // Public functions
+    // ...
+
+    // Internal functions
+    // ...
+
+    // Private functions
+    // ...
+
 }
